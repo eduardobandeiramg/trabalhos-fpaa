029034ea7c555d16e61c5480fe0fc515b3ae6593
eduardobandeiramg <eduardobandeiramg@gmail.com>
2025-10-01 19:33:15 -0300

    acessando arestas

--- 3_caminho_hamiltoniano/code/main.py
+++ 3_caminho_hamiltoniano/code/main.py
@@ -7,6 +7,10 @@ import matplotlib.pyplot as plt
 # Criando o grafo aleatório:
 G = nx.gnp_random_graph(nos, 1)
 
+# Acessando arestas:
+print(G.edges)
+
 # Desenhando o grafo:
-nx.draw(G, with_labels=True, font_weight='bold')
+nx.draw(G, with_labels=True, font_weight='bold', font_color="white")
 plt.show()
+
